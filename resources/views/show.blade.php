79fc612d14643c324537e17e9d62892470d84019
johndomasig22 <johndomasig212@gmail.com>
2024-03-19 04:26:32 +0800

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,9 +7,9 @@
     <form method="post" action="{{ url("qr") }}">
         @csrf
         <label class="form-label">Input Text:</label>
-        <input type="text" class="form-control" name="qr_text" />
+        <input type="text" class="form-control" name="qr_text"  autocomplete="off"/>
 
-        <input type="submit" class="btn btn-primary mt-2" value="Generate QR" />
+        <input type="submit" class="btn btn-primary mt-2" value="Generate QR"/>
     </form>
 
     @if($value!="")
